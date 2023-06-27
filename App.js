babe7f60873e4555d95e3d4b08a315a7fe8faa47
Daniel Soto <dasg.1993@gmail.com>
2023-06-27 18:04:29 -0500

    [feature] - handle task and task list in app js and implement input and map component with styles

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { styles } from './styles';
 import { useState } from 'react';
 
 export default function App() {
+  const [task, setTask] = useState('')
+  const [tasks, setTasks] = useState([])
   const [borderColor, setBorderColor] = useState('#C5C9E7')
 
   const onHandlerFocus = () => {
@@ -11,6 +13,22 @@ export default function App() {
 
   const onHandlerBlur = () => {
     setBorderColor('#C5C9E7')
+  }
+
+  const onHandlerChangeText = (text) => {
+    setTask(text)
+  }
+
+  const onHandlerCreateTask = () => {
+    setTasks([
+      ...tasks,
+      {
+        id: Date.now().toString(),
+        value: task
+      }
+    ])
+
+    setTask('')
   }
   return (
     <SafeAreaView style={styles.safeArea}>
@@ -26,8 +44,19 @@ export default function App() {
             placeholderTextColor='#C5C9E7'
             onFocus={onHandlerFocus}
             onBlur={onHandlerBlur}
+            onChangeText={onHandlerChangeText}
+            value={task}
           />
-          <Button title='Create' color='#424D9E' />
+          <Button disabled={task.length === 0} title='Create' color='#424D9E' onPress={onHandlerCreateTask}/>
+        </View>
+        <View style={styles.listContainer}>
+          {
+            tasks.map((item) => (
+              <View style={styles.containerItem} key={item.id}>
+                <Text style={styles.listItem}>{item.value}</Text>
+              </View>
+            ))
+          }
         </View>
       </View>
     </SafeAreaView>
--- styles.js
+++ styles.js
@@ -21,6 +21,33 @@ export const styles = StyleSheet.create({
       paddingHorizontal: 10,
       color: '#212121',
       fontSize: 14,
-    }
+    },
+    listContainer: {
+      marginTop: 25,
+      gap: 15,
+    },
+    containerItem: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      paddingVertical: 20,
+      backgroundColor: '#D4D7ED',
+      paddingHorizontal: 10,
+      borderRadius: 10,
+      color: '#212121',
+      shadowColor: "#000",
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.22,
+      shadowRadius: 2.22,
+      elevation: 3,
+    },
+    listItem: {
+      fontSize: 15,
+      fontWeight: 'bold',
+      color: '#212121',
+    },
   });
   
