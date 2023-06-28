2366988caa266aaae471ae8c2aa01c47db266040
Daniel Soto <dasg.1993@gmail.com>
2023-06-27 19:07:08 -0500

    [feature] - add pressable item to delete item list and add modal with content and styles

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView } from 'react-native';
+import { Modal, StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
 import { styles } from './styles';
 import { useState } from 'react';
 
 export default function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  const [isVisible, setIsVisible] = useState(false)
+  const [selectedTask, setSelectedTask] = useState(null)
   const [borderColor, setBorderColor] = useState('#C5C9E7')
 
   const onHandlerFocus = () => {
@@ -30,6 +32,25 @@ export default function App() {
 
     setTask('')
   }
+
+  const onHandlerModal = (item) => {
+    setIsVisible(true)
+    setSelectedTask(item)
+  }
+
+  const onHandleDelete = (id) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id))
+    setIsVisible(false)
+  }
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => onHandlerModal(item)} style={styles.containerItem}>
+      <Text style={styles.listItem}>{item.value}</Text>
+      <Text style={styles.icon}>X</Text>
+    </TouchableOpacity>
+  )
+
+  
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
@@ -49,16 +70,36 @@ export default function App() {
           />
           <Button disabled={task.length === 0} title='Create' color='#424D9E' onPress={onHandlerCreateTask}/>
         </View>
-        <View style={styles.listContainer}>
-          {
-            tasks.map((item) => (
-              <View style={styles.containerItem} key={item.id}>
-                <Text style={styles.listItem}>{item.value}</Text>
-              </View>
-            ))
-          }
-        </View>
+          <FlatList
+            data={tasks}
+            renderItem={renderItem}
+            style={styles.listContainer}
+            contentContainerStyle={styles.list}
+            alwaysBounceVertical={false}
+            keyExtractor={item => item.id}
+          />
       </View>
+      <Modal visible={isVisible} animationType='slide'>
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalTitle}>Task Detail</Text>
+          <View style={styles.modalDetailContainer}>
+            <Text style={styles.modalDetailMessage}>Are you sure to delete this item?</Text>
+            <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
+          </View>
+          <View style={styles.modalButtonContainer}>
+            <Button 
+              title='Cancel'
+              color='#424D9E'
+              onPress={() =>  setIsVisible(false)}
+            />
+            <Button 
+              title='Delete'
+              color='red'
+              onPress={() => onHandleDelete(selectedTask?.id)}
+            />
+          </View>
+        </View>
+      </Modal>
     </SafeAreaView>
   );
 }
--- styles.js
+++ styles.js
@@ -24,7 +24,6 @@ export const styles = StyleSheet.create({
     },
     listContainer: {
       marginTop: 25,
-      gap: 15,
     },
     containerItem: {
       flexDirection: 'row',
@@ -48,6 +47,45 @@ export const styles = StyleSheet.create({
       fontSize: 15,
       fontWeight: 'bold',
       color: '#212121',
+    },
+    list: {
+      gap: 15,
+      paddingBottom: 20,
+    },
+    icon: {
+      color: 'red',
+      fontSize: 16,
+    },
+    modalContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 60,
+      paddingVertical: 20,
+    },
+    modalTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 10,
+    },
+    modalDetailContainer: {
+      paddingVertical: 20,
+    },
+    modalDetailMessage: {
+      fontSize: 14,
+      color: '#212121',
+    },
+    selectedTask: {
+      fontSize: 14,
+      color: '#212121',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      paddingVertical: 10,
+    },
+    modalButtonContainer: {
+      flexDirection: 'row',
+      width: '100%',
+      justifyContent: 'space-around',
+      marginHorizontal: 20,
     },
   });
   
