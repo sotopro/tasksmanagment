494886250775f2f743c52bceb4904506859b9ca6
Daniel Soto <dasg.1993@gmail.com>
2023-06-29 18:04:27 -0500

    [setup] -  add eslint prettier

--- App.js
+++ App.js
@@ -1,85 +1,101 @@
-import { Modal, StyleSheet, Text, View, TextInput, Button, StatusBar, SafeAreaView, FlatList, TouchableOpacity } from 'react-native';
-import { styles } from './styles';
-import { useState } from 'react';
+import { useState } from "react";
+import {
+  Modal,
+  Text,
+  View,
+  TextInput,
+  Button,
+  SafeAreaView,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+
+import { styles } from "./styles";
 
 export default function App() {
-  const [task, setTask] = useState('')
-  const [tasks, setTasks] = useState([])
-  const [isVisible, setIsVisible] = useState(false)
-  const [selectedTask, setSelectedTask] = useState(null)
-  const [borderColor, setBorderColor] = useState('#C5C9E7')
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isVisible, setIsVisible] = useState(false);
+  const [selectedTask, setSelectedTask] = useState(null);
+  const [borderColor, setBorderColor] = useState("#C5C9E7");
 
   const onHandlerFocus = () => {
-    setBorderColor('#424D9E')
-  }
+    setBorderColor("#424D9E");
+  };
 
   const onHandlerBlur = () => {
-    setBorderColor('#C5C9E7')
-  }
+    setBorderColor("#C5C9E7");
+  };
 
   const onHandlerChangeText = (text) => {
-    setTask(text)
-  }
+    setTask(text);
+  };
 
   const onHandlerCreateTask = () => {
+    console.log("onHandlerCreateTask");
+    console.warn({ task });
     setTasks([
       ...tasks,
       {
         id: Date.now().toString(),
-        value: task
-      }
-    ])
+        value: task,
+      },
+    ]);
 
-    setTask('')
-  }
+    setTask("");
+  };
 
   const onHandlerModal = (item) => {
-    setIsVisible(true)
-    setSelectedTask(item)
-  }
+    setIsVisible(true);
+    setSelectedTask(item);
+  };
 
   const onHandleDelete = (id) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id))
-    setIsVisible(false)
-  }
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
+    setIsVisible(false);
+  };
 
   const renderItem = ({ item }) => (
     <TouchableOpacity onPress={() => onHandlerModal(item)} style={styles.containerItem}>
       <Text style={styles.listItem}>{item.value}</Text>
       <Text style={styles.icon}>X</Text>
     </TouchableOpacity>
-  )
+  );
 
-  
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <View style={styles.inputContainer}>
-          <TextInput 
-            style={[styles.input, { borderColor: borderColor}]} 
-            placeholder='add new tasks'
-            autoCapitalize='none'
+          <TextInput
+            style={[styles.input, { borderColor }]}
+            placeholder="add new tasks"
+            autoCapitalize="none"
             autoCorrect={false}
-            cursorColor='#424D9E'
-            selectionColor='#D4D7ED'
-            placeholderTextColor='#C5C9E7'
+            cursorColor="#424D9E"
+            selectionColor="#D4D7ED"
+            placeholderTextColor="#C5C9E7"
             onFocus={onHandlerFocus}
             onBlur={onHandlerBlur}
             onChangeText={onHandlerChangeText}
             value={task}
           />
-          <Button disabled={task.length === 0} title='Create' color='#424D9E' onPress={onHandlerCreateTask}/>
-        </View>
-          <FlatList
-            data={tasks}
-            renderItem={renderItem}
-            style={styles.listContainer}
-            contentContainerStyle={styles.list}
-            alwaysBounceVertical={false}
-            keyExtractor={item => item.id}
+          <Button
+            disabled={task.length === 0}
+            title="Create"
+            color="#424D9E"
+            onPress={onHandlerCreateTask}
           />
+        </View>
+        <FlatList
+          data={tasks}
+          renderItem={renderItem}
+          style={styles.listContainer}
+          contentContainerStyle={styles.list}
+          alwaysBounceVertical={false}
+          keyExtractor={(item) => item.id}
+        />
       </View>
-      <Modal visible={isVisible} animationType='slide'>
+      <Modal visible={isVisible} animationType="slide">
         <View style={styles.modalContainer}>
           <Text style={styles.modalTitle}>Task Detail</Text>
           <View style={styles.modalDetailContainer}>
@@ -87,16 +103,8 @@ export default function App() {
             <Text style={styles.selectedTask}>{selectedTask?.value}</Text>
           </View>
           <View style={styles.modalButtonContainer}>
-            <Button 
-              title='Cancel'
-              color='#424D9E'
-              onPress={() =>  setIsVisible(false)}
-            />
-            <Button 
-              title='Delete'
-              color='red'
-              onPress={() => onHandleDelete(selectedTask?.id)}
-            />
+            <Button title="Cancel" color="#424D9E" onPress={() => setIsVisible(false)} />
+            <Button title="Delete" color="red" onPress={() => onHandleDelete(selectedTask?.id)} />
           </View>
         </View>
       </Modal>
--- styles.js
+++ styles.js
@@ -1,91 +1,90 @@
 import { StyleSheet, StatusBar } from "react-native";
 
 export const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
+  safeArea: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    marginHorizontal: 20,
+    paddingTop: StatusBar.currentHeight,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  input: {
+    flex: 0.95,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    color: "#212121",
+    fontSize: 14,
+  },
+  listContainer: {
+    marginTop: 25,
+  },
+  containerItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 20,
+    backgroundColor: "#D4D7ED",
+    paddingHorizontal: 10,
+    borderRadius: 10,
+    color: "#212121",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      marginHorizontal: 20,
-      paddingTop: StatusBar.currentHeight
-    },
-    inputContainer: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-    },
-    input: {
-      flex: 0.95,
-      borderWidth: 1,
-      borderRadius: 10,
-      paddingHorizontal: 10,
-      color: '#212121',
-      fontSize: 14,
-    },
-    listContainer: {
-      marginTop: 25,
-    },
-    containerItem: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      paddingVertical: 20,
-      backgroundColor: '#D4D7ED',
-      paddingHorizontal: 10,
-      borderRadius: 10,
-      color: '#212121',
-      shadowColor: "#000",
-      shadowOffset: {
-        width: 0,
-        height: 1,
-      },
-      shadowOpacity: 0.22,
-      shadowRadius: 2.22,
-      elevation: 3,
-    },
-    listItem: {
-      fontSize: 15,
-      fontWeight: 'bold',
-      color: '#212121',
-    },
-    list: {
-      gap: 15,
-      paddingBottom: 20,
-    },
-    icon: {
-      color: 'red',
-      fontSize: 16,
-    },
-    modalContainer: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      marginTop: 60,
-      paddingVertical: 20,
-    },
-    modalTitle: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 10,
-    },
-    modalDetailContainer: {
-      paddingVertical: 20,
-    },
-    modalDetailMessage: {
-      fontSize: 14,
-      color: '#212121',
-    },
-    selectedTask: {
-      fontSize: 14,
-      color: '#212121',
-      fontWeight: 'bold',
-      textAlign: 'center',
-      paddingVertical: 10,
-    },
-    modalButtonContainer: {
-      flexDirection: 'row',
-      width: '100%',
-      justifyContent: 'space-around',
-      marginHorizontal: 20,
-    },
-  });
-  
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+  },
+  listItem: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#212121",
+  },
+  list: {
+    gap: 15,
+    paddingBottom: 20,
+  },
+  icon: {
+    color: "red",
+    fontSize: 16,
+  },
+  modalContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 60,
+    paddingVertical: 20,
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  modalDetailContainer: {
+    paddingVertical: 20,
+  },
+  modalDetailMessage: {
+    fontSize: 14,
+    color: "#212121",
+  },
+  selectedTask: {
+    fontSize: 14,
+    color: "#212121",
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingVertical: 10,
+  },
+  modalButtonContainer: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around",
+    marginHorizontal: 20,
+  },
+});
